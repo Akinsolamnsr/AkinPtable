4ee51eef328be32783857d54324b93b204cc95c8
Akinsola <akinsolamnsr@gmail.com>
2023-04-13 21:48:49 +0100

    4/13/23

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import {AppWrapper} from "../components/UseContext"
 import { SessionProvider } from "next-auth/react"
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-
+import {DefaultSeo} from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   const router=useRouter()
@@ -11,6 +11,16 @@ function MyApp({ Component, pageProps }) {
   return(
     <AppWrapper>
       <SessionProvider session={pageProps.session}>
+      <DefaultSeo
+                title="Periodic Table and Games"
+                description="Interactive periodic table showing properties and tale trends. It has Game section which helps to know element and symbol,Electronic Configuration, Table trend, Family and Block with scoreboard o measyre progress with peers"
+                openGraph={{
+                    type: 'website',
+                    locale: 'en_IE',
+                    url: 'https://atableslive.com/',
+                    siteName: 'Atableslive',
+                }}
+            />
       <Component {...pageProps} />
       </SessionProvider>
     
