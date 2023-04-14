7f9705751ef237e2e85df20207f5e5cf0da989a6
Akinsola <akinsolamnsr@gmail.com>
2023-04-14 22:08:36 +0100

    4/14/23

--- components/PageThree/NavWrapNew.js
+++ components/PageThree/NavWrapNew.js
@@ -45,7 +45,7 @@ export default function NavWrapNew({children}) {
         <div className='w-1/3 flex justify-between h-full '>
         
         <span className='hover:text-cyan-500 flex items-center'>
-           {size.width<550?(<><Link href="/"><a><Image src={homePic} alt="pics" width="25" heigth="25" /></a></Link></>):(<><Image src={homePic} alt="pics" width="25" heigth="25"  /><span className={` h-full pl-[1.5rem] pr-[1.2rem] -ml-[1.5rem] flex items-center   ${router.pathname == "/" ? "border-b-4 border-cyan-700 bg-gray-100" : ""}`}><Link href="/"><a> Home</a></Link></span></>)}
+           {size.width<550?(<><Link href="/"><a><Image src={homePic} alt="pics" width="25" heigth="25" /></a></Link></>):(<><Image src={homePic} alt="pics" width="25" heigth="25"  /><span className={` h-full pl-[1.5rem] pr-[1.2rem] -ml-[1.5rem] flex items-center   ${router.pathname == "/home" ? "border-b-4 border-cyan-700 bg-gray-100" : ""}`}><Link href="/"><a> Home</a></Link></span></>)}
            </span>
           <span className='hover:text-cyan-500 flex items-center'>
           {size.width<550?(<><Link href="/Properties"><a><Image src={PropPic} alt="pics" width="25" heigth="25" /></a></Link></>):(<><Image src={PropPic} alt="pics" width="25" heigth="25"   /><span className={`h-full  flex items-center pl-[1.5rem] pr-[1.2rem] -ml-[1.5rem] ${router.pathname == "/Properties/" ? "border-b-4 border-cyan-700 bg-gray-100" : ""}`}><Link href="/Properties"><a>Properties</a></Link></span></>)}
